ee07b6ae6d3f061ca959aa9ef7fea00ed89b816d
everthis <wwwaap@gmail.com>
2015-06-04 17:04:09 +0800

    filter added.

--- app/scripts/query_list.js
+++ app/scripts/query_list.js
@@ -1,6 +1,7 @@
 'use strict';
 var doc = document,
-    _preserve_btns = doc.querySelectorAll('.preserve_btn');
+    _preserve_btns = doc.querySelectorAll('.preserve_btn'),
+    _filter_btn = doc.querySelector('.filter_btn');
 
 (function() {
 	[].map.call(_preserve_btns, function(el) {
@@ -13,4 +14,9 @@ var doc = document,
 var _header = doc.querySelector('.header .logo');
 _header.addEventListener('click', function() {
 	window.location.href = '/main.html';
+});
+_filter_btn.addEventListener('click', function() {
+	var filter_shadow = doc.querySelector('.shadow.filter');
+	filter_shadow.classList.remove('hide');
+
 });
